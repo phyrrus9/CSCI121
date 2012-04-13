f82aad48e99819eaf80ff33468dd2f8d5fe74390
Ethan Laur <phyrrus9@gmail.com>
2012-04-12 22:20:25 -0400

    Added race car capability and esplosions

--- Assignment14/main.cpp
+++ Assignment14/main.cpp
@@ -1,3 +1,8 @@
+/* Assignment 14 by Ethan Laur due Wednesday 18, 2012
+ * This program will create a new instance of a car,
+ * and increase its speed 5 times.  The class (though
+ * commented out) has the ability for faster and slower
+ * cars, just in case you wanted to race... */
 #include <iostream>
 #include <stdio.h>
 #include <string.h>
@@ -5,12 +10,18 @@
 using namespace std;
 class Car
 {
+private: //just for the sake of making sure we did this
     int yearModel;
     string make;
     int speed;
+    //int acceleration; //the rate at which the car accelerates
+    //int brakespeed; //the speed at which the car slows down
+    //int position; //sample position in a race
 public:
     Car(int, string);
+    //Car(int,int,int,string); //year,acceleration,brakespeed,make
     int getmodel(void);
+    //int getposition(void); //for the racecar version
     string getmake(void);
     int getspeed(void);
     void accelerate(void);
@@ -23,10 +34,23 @@ Car::Car(int _, string __)
     speed = 0;
     cout << "New car created: " << yearModel << " " << make << endl;
 }
+/*Car::Car(int _,int __,int ___, string ____) //for the racecar version
+{
+    yearModel = _;
+    acceleration = __;
+    brakespeed = ___
+    make = ____;
+    speed = 0;
+    cout << "New car created: " << yearModel << " " << make << endl;
+}*/
 int Car::getmodel(void)
 {
     return yearModel;
 }
+/*int Car::getposition(void) //returns the position for the racecars
+{
+    return position;
+}*/
 string Car::getmake(void)
 {
     return make;
@@ -38,10 +62,12 @@ int Car::getspeed(void)
 void Car::accelerate(void)
 {
     speed += 5;
+    //speed += acceleration; //for the racecar version
 }
 void Car::brake(void)
 {
     speed -= 5;
+    //speed -= brakespeed; //for the racecar version
 }
 int main()
 {
@@ -58,5 +84,6 @@ int main()
         rlyfastcar->brake();
         cout << rlyfastcar->getspeed() << endl;
     }
+    delete rlyfastcar; //make the car go boom boom
     return 0;
 }
